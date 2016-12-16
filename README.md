22f1992a53b8e425079142a50b95d29433486719
Brad Taniguchi <bradtaniguchi@gmail.com>
2016-12-16 11:12:48 -0800

    First Commit
    added README.md and app.sh, basics. app.sh is an empty file

---
+++ README.md
@@ -0,0 +1,5 @@
+**RoomRenter**
+A web-based solution to handling temporary room rentals.
+Utilizing Nodejs, and Node-Webkit(nwjs).
+
+
---
+++ app.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# This script is used to start the program on linux machines
+# use the alternate app.exe script to run the program on windows machines.
+# no one has a mac, so there isn't a run file for it, sorry!

ab26f03470244cd50ddad01a21b53708ab53ac7f
Brad Taniguchi <bradtaniguchi@gmail.com>
2016-12-16 14:02:30 -0800

    Added run definition to app.sh, now runs program.
    By using ./app.sh in the terminal you SHOULD start the program IF you
    installed nodejs correctly. Everything else is portable!

--- app.sh
+++ app.sh
@@ -2,3 +2,5 @@
 # This script is used to start the program on linux machines
 # use the alternate app.exe script to run the program on windows machines.
 # no one has a mac, so there isn't a run file for it, sorry!
+
+./node_modules/nw/nwjs/nw .

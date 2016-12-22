ff7ffdfc0fc84168212275b64e54c89c0861c91f
Brad <bradtaniguchi@gmail.com>
2016-12-22 14:03:08 -0800

    Created working Navbar
    The nav will transfer you to atleast the admin module

--- app/shared/top-navbar/navbarController.js
+++ app/shared/top-navbar/navbarController.js
@@ -1,3 +1,24 @@
 /**
  * Created by brad on 12/22/16.
  */
+
+roomRenter.controller("navbarController", function($scope, $location){
+    //a simple debugging variable to make sure we are in the right controller
+    $scope.name = 'navbarController name';
+
+    $scope.showBack = function() {
+        return $location.path == '/Admin';
+    };
+
+    $scope.go = function (path) {
+        $location.path(path);
+    };
+
+    $scope.back = function () {
+        window.history.back();
+    };
+
+    $scope.isActive = function (route) {
+        return route === $location.path();
+    };
+});

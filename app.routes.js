d1925acdda1b229a7ef3080e38d9a6e0a7b332c4
Brad <bradtaniguchi@gmail.com>
2016-12-22 12:51:22 -0800

    Added all routes to the current views
    The View files are all pretty much blank ATM. Will start testing them
    now.

--- app.routes.js
+++ app.routes.js
@@ -1,24 +1,32 @@
 /**
  * Created by brad on 12/22/16.
+ * This javascript file handles all the routing of the application.
+ * Because its a node-webkit app, the URLs are hidden. But I have given them
+ * logical names to help us during debugging. More can be added at
+ * NOTE: All controllers/views are declared alphabetically.
  */
 roomRenter.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-    when('/ClockIn', {
-        templateUrl: '/templates/ClockIn.html',
-        controller: 'clockInController'
-    })/*.when('/ClockOut',{
-        templateUrl: '/templates/ClockOut.html',
-        controller: 'clockOutController'
-    }).when('/Admin', {
-        templateUrl: '/templates/Admin.html',
-        controller: 'adminController'
-    }).when('/Admin/ViewTimes',{
-        templateUrl: '/templates/ViewTimes.html',
-        controller: 'viewTimesController'
-    }).when('Admin/ViewStats',{
-        templateUrl: '/templates/ViewStats.html',
-        controller: 'viewStatsController'
-    })*/.otherwise({
+    when('/Admin/Export', {
+        templateUrl: 'app/components/adminExport/adminExportView.html',
+        controller: 'adminExportController'
+    }).when('/Admin',{
+        templateUrl: 'app/components/adminMain/adminMainView.html',
+        controller: ''
+    }).when('/Admin/Stat', {
+        templateUrl: 'app/components/adminStat/adminStatView.html',
+        controller: ''
+    }).when('/Admin/Time',{
+        templateUrl: 'app/components/adminTime/adminTimeView.html',
+        controller: ''
+
+    }).when('/ClockIn',{
+        templateUrl: 'app/components/clockIn/clockInView.html',
+        controller: ''
+    }).when('/ClockOut',{
+        templateUrl: 'app/components/clockOut/clockOutView.html',
+        controller: ''
+    }).otherwise({
         templateUrl: 'app/components/main/mainView.html',
         controller: 'mainController'
     });

bc8b81f4d38bc78ca462f10deebc56e7ca15bb15
bradtaniguchi <bradtaniguchi@gmail.com>
2016-12-27 17:57:54 -0800

    Added basic AdminMain controller.
    Right now this controller only handles a redirect and provides the number
    of rooms the application is currently configured to. More configurations
    can be added at a later time.

--- app/components/adminMain/adminMainController.js
+++ app/components/adminMain/adminMainController.js
@@ -1,3 +1,12 @@
 /**
  * Created by brad on 12/22/16.
  */
+
+roomRenter.controller('adminMainController', function($scope, generalService, appInfo) {
+    $scope.numberOfRooms = appInfo.numberOfRooms; //get the number of Rooms
+
+    /*function to call to change the application location*/
+    $scope.go = function(path) {
+        generalService.changeView(path);
+    };
+});

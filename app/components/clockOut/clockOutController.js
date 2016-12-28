b61d806b109f82306a0dabe24807e9cebb6be310
Vigeneareay Phriang <vphriang21@gmail.com>
2016-12-28 00:03:57 -0800

    deleted non working code

--- app/components/clockOut/clockOutController.js
+++ app/components/clockOut/clockOutController.js
@@ -5,12 +5,4 @@
 
 roomRenter.controller('clockOutController', function ($scope) {
 
-    $scope.submit=function () {
-
-
-    }
-    document.getElementById("room1").onclick = function() {
-        location.href = "/home/vidget/WebstormProjects/Proj1/RoomRenter/app/components/main/mainView.html";
-    }
-
 });

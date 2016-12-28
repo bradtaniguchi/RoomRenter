9f0736189be757f018a04bc4826206a2dd98a9cd
bradtaniguchi <bradtaniguchi@gmail.com>
2016-12-28 15:57:16 -0800

    Added part of the Dynamic rooms to Clockout
    Now the page can handle dynamic rooms. But because it gets the amount of
    rooms from the database service, it currently is hardcoded to 5 rooms. We
    may need to change the input fields in the middle, as they should be un
    editable.

--- app/components/clockOut/clockOutController.js
+++ app/components/clockOut/clockOutController.js
@@ -3,6 +3,57 @@
  */
 
 
-roomRenter.controller('clockOutController', function ($scope) {
+roomRenter.controller('clockOutController', function ($scope, generalService, appInfo) {
+    $scope.room = 0;
+    $scope.numberOfRooms = appInfo.numberOfRooms;
+    $scope.usedRooms = [];  //the rooms currently in use, thus the ones we are to display
+    $scope.alertClass = ""; //shows the alert message if the rooms are empty
+    $scope.emtpyMessage = ""; //only shows if there are no rooms
 
+    /*To provide navagation from this page*/
+    $scope.go = function(path) {
+        generalService.changeView(path);
+    };
+
+    /*Close the entry alert on the page*/
+    $scope.alertClose = function() {
+        $scope.alertClass = "hide";
+        $scope.emptyMessage = "";
+    };
+
+    /*Open the entry alert on the page, with the given message*/
+    $scope.alertOpen = function(message){
+        $scope.alertClass = "";
+        $scope.emptyMessage = message;
+    };
+
+    /*To create the rooms we need to reference the database to see who is clocked in.*/
+    function getUsedRooms() {
+        console.log("Getting the currently Used Rooms...");
+        /*make a service call here, until then we will hard code it*/
+
+        /*The actual database call will provide the following:
+        * 1. Username
+        * 2. Room
+        * 3. ClockIn Time*/
+        $scope.usedRooms = new Array(5); //5 rooms are in use ATM, just for a UI test
+        if ($scope.usedRooms == 0) { /*There are no rooms to be clocked out of, so show a different message*/
+            $scope.alertOpen("There are no rooms being used! Please hit cancel to go back.");
+
+        } else {
+            $scope.alertClose();
+        }
+    }
+
+    /*This is when the user clicks on the Clockout button.
+    * We call a database function to clock the user out, and then open a confirm modal.
+    * This modal will just clarify the information we got from the database.*/
+    $scope.clockOut = function(room) {
+        console.log("Clocking out of room :" + room);
+        /*call database service here*/
+
+        /*open modal*/
+    };
+
+    getUsedRooms();
 });

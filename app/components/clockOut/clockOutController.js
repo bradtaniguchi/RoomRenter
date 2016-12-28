3cc21394392ace9a691807450e6e04b1da500909
Vigeneareay Phriang <vphriang21@gmail.com>
2016-12-27 23:59:20 -0800

    added javascript features to available time and open rooms function in main.html

--- app/components/clockOut/clockOutController.js
+++ app/components/clockOut/clockOutController.js
@@ -2,3 +2,15 @@
  * Created by brad on 12/22/16.
  */
 
+
+roomRenter.controller('clockOutController', function ($scope) {
+
+    $scope.submit=function () {
+
+
+    }
+    document.getElementById("room1").onclick = function() {
+        location.href = "/home/vidget/WebstormProjects/Proj1/RoomRenter/app/components/main/mainView.html";
+    }
+
+});
--- app/components/main/mainController.js
+++ app/components/main/mainController.js
@@ -1,8 +1,8 @@
-/**
+ /**
  * Created by brad on 12/16/16.
  */
 
-roomRenter.controller("mainController", function($scope, $location){
+roomRenter.controller("mainController", function($scope, $location) {
     $scope.name = "";
     $scope.back = function () {
         window.history.back();
@@ -16,7 +16,15 @@ roomRenter.controller("mainController", function($scope, $location){
         return ($location.path()).indexOf(route) >= 0;
     };
 
-    $scope.testDatabase = function(){
+    $scope.testDatabase = function () {
         console.log(database.testGetTables()); //should return 2 in console!
     }
+
+    $scope.vacant = function() {
+        document.getElementById("avail").innerHTML = "1 2 3 4 5";
+    }
+    $scope.time = function() {
+        document.getElementById("opens").innerHTML = Date();
+    }
+
 });

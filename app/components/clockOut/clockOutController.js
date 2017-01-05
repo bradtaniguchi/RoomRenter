cf642746962ab0ff6d37378058556a72cedcc021
Brad <bradtaniguchi@gmail.com>
2017-01-05 11:41:20 -0800

    ClockOut updates using database information
    Now the clockout page updates the view depending on who is clocked in.
    There still needs to be UI changes and the clockout buttons still need
    to be fixed.

--- app/components/clockOut/clockOutController.js
+++ app/components/clockOut/clockOutController.js
@@ -3,14 +3,14 @@
  */
 
 
-roomRenter.controller('clockOutController', function ($scope, generalService, appInfo) {
+roomRenter.controller('clockOutController', function ($scope, generalService, appInfo, database) {
     $scope.room = 0;
     $scope.numberOfRooms = appInfo.numberOfRooms;
     $scope.usedRooms = [];  //the rooms currently in use, thus the ones we are to display
     $scope.alertClass = ""; //shows the alert message if the rooms are empty
     $scope.emtpyMessage = ""; //only shows if there are no rooms
 
-    /*To provide navagation from this page*/
+    /*To provide navigation from this page*/
     $scope.go = function(path) {
         generalService.changeView(path);
     };
@@ -30,19 +30,32 @@ roomRenter.controller('clockOutController', function ($scope, generalService, ap
     /*To create the rooms we need to reference the database to see who is clocked in.*/
     function getUsedRooms() {
         console.log("Getting the currently Used Rooms...");
-        /*make a service call here, until then we will hard code it*/
-
-        /*The actual database call will provide the following:
-        * 1. Username
-        * 2. Room
-        * 3. ClockIn Time*/
-        $scope.usedRooms = new Array(5); //5 rooms are in use ATM, just for a UI test
-        if ($scope.usedRooms == 0) { /*There are no rooms to be clocked out of, so show a different message*/
-            $scope.alertOpen("There are no rooms being used! Please hit cancel to go back.");
-
-        } else {
-            $scope.alertClose();
-        }
+        /*Here we get the database values to be put onto the page rooms*/
+        database.getUsersLoggedIn(function(Users) {
+            if(Users.length > 0) {
+                $scope.alertClose();
+                /*Users is a list of strings, each string corresponds to a user.*/
+                console.log("clockOutController: users logged in: " + JSON.stringify(Users));
+                for (var i = 0 ; i < Users.length; i++) {
+                    /*we first need to get the User object from the database, not just their username*/
+                    database.getUserLoggedIn(Users[i], function(User) {
+                        /*for each User object, we need to get their room number being used*/
+                        database.getRoomLoggedIn(User, function(roomNumber){
+                            /*With this information, we need to create a room for the usedRoom array*/
+                            console.log("creating room logged in " + roomNumber);
+                            var usedRoom = {
+                                "roomNumber" : roomNumber, //this is the room the user is logged into
+                                "username" : User.username //this is the users username, to display who they are
+                                //"usageTime" : 0 //this is how long the user has been in the room, use the start time
+                            };
+                            $scope.usedRooms.push(usedRoom); //push this room onto the page
+                        });
+                    });
+                }
+            } else { //there are no people clocked in!
+                $scope.alertOpen("There are no rooms being used! Please hit cancel to go back.");
+            }
+        });
     }
 
     /*This is when the user clicks on the Clockout button.

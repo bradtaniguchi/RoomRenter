ce69e1459dd4547edc974bc02701ed349b2563dc
Brad Taniguchi <bradtaniguchi@gmail.com>
2017-01-10 16:34:30 -0800

    Integrated clipboard.js for ExportView
    Now the Admin can copy the entire database status via a simple button
    click. If we ever get around to printing to files, then we can add the
    print button here.

--- app/components/adminExport/adminExportController.js
+++ app/components/adminExport/adminExportController.js
@@ -4,6 +4,7 @@
 
 roomRenter.controller('adminExportController', function($scope, moment, database){
     $scope.message = "[Database Message Building...]";
+    $scope.alertClass = "hide"; //Default starts as hide
 
     /*sorting function to pass to sort function.*/
     var sortBySeconds = function(a,b) {
@@ -12,11 +13,22 @@ roomRenter.controller('adminExportController', function($scope, moment, database
         return 0;
     };
 
+    /*Close the entry alert on the page*/
+    $scope.alertClose = function() {
+        $scope.alertClass = "hide";
+    };
+
+    /*Open the entry alert on the page, with the given message*/
+    $scope.alertOpen = function() {
+        $scope.alertClass = "";
+    };
+
     /*Builds the database message*/
     $scope.buildMessage = function() {
         /*call the database to get a list of all entries*/
         $scope.message = "[Database Message Building...]"; //change back to default while we load
         database.getEntries(function(entries){
+            entries.sort(sortBySeconds()); //sort the array
             $scope.message = "";
             /*With the list of entry objects, 'stringify' them*/
             entries.forEach(function(entry){
@@ -26,4 +38,8 @@ roomRenter.controller('adminExportController', function($scope, moment, database
         });
     };
     $scope.buildMessage();
+    /*Init the copy button*/
+    (function(){
+        new Clipboard('#copy-button');
+    })();
 });

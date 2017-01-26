ca58eb81e69d09304e1c7ebb85f536798651c5d5
Vigeneareay Phriang <vphriang21@gmail.com>
2017-01-26 12:53:04 -0800

    instantiated clockIn function

--- app/services/clockIn.service.js
+++ app/services/clockIn.service.js
@@ -3,5 +3,17 @@
   /*Bradley Taniguchi
   *24/01/2017
   */
+  angular.module('roomRenter').factory('clockIn', clockIn);
+  clockIn.$inject=['$log', 'constants', '$localForage'];
+
+  function clockIn($log, constants, $localForage){
+    return {
+      clockIn : clockIn
+    };
+    function clockIn(userID, username, room){
+      $log.log('User ID: '+userID, ' Username: '+ username, ' Room number: '+ room);
+
+    }
+  }
 
 })();

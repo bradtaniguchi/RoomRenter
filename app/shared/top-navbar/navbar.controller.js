782359556bdaadb33263c42ddfb617f4ec68c4ef
Brad <bradtaniguchi@gmail.com>
2017-01-29 14:21:46 -0800

    Added quick check button to navbar
    it prints the entire database to the console

--- app/shared/top-navbar/navbar.controller.js
+++ app/shared/top-navbar/navbar.controller.js
@@ -2,9 +2,9 @@
   'use strict';
 
   angular.module('roomRenter').controller('navbarController', navbarController);
-  navbarController.$inject = ['$log', 'constants', 'generalService'];
+  navbarController.$inject = ['$log', 'constants', 'generalService', '$localForage'];
 
-  function navbarController($log, constants, generalService) {
+  function navbarController($log, constants, generalService, $localForage) {
     var vm = this;
     //vm.name = "navbarController name"; //wtf does this do?
     //vm.swapMessage = "..."; //wtf does THIS do?
@@ -14,6 +14,7 @@
     vm.refresh = refresh;
     vm.go = go;
     vm.back = back;
+    vm.quickPrint = quickPrint;
     //vm.swapRooms;
 
     return vm;
@@ -27,6 +28,25 @@
     }
     function back() {
       window.history.back();
+    }
+    /*quick function to print important items in the database*/
+    function quickPrint() {
+      $log.log("******DEBUG PRINTOUT******");
+      $localForage.getItem(constants.RESERVED_DATABASE_NAME).then(function(object){
+        $log.log("Database object: " + JSON.stringify(object));
+      });
+      /*get ALL items in database*/
+      // Find the number of items in the datastore.
+      $localForage.length().then(function(length) {
+          // Loop over each of the items.
+          for (var i = 0; i < length; i++) {
+            $localForage.key(i).then(function(key) {
+              $localForage.getItem(key).then(function(item){
+                 $log.log("   key: " + key + " value: "+ JSON.stringify(item));
+               });
+            });
+          }
+      });
     }
     /*theres a build rooms function, I don't need it I guess*/
   }

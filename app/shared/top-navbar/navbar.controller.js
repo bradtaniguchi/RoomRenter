29aa0cd8993f2051ec792b23209abe385b0d1374
Brad <bradtaniguchi@gmail.com>
2017-01-31 10:16:08 -0800

    Added another checker for who is logged in.
    This will be removed in production

--- app/shared/top-navbar/navbar.controller.js
+++ app/shared/top-navbar/navbar.controller.js
@@ -2,9 +2,10 @@
   'use strict';
 
   angular.module('roomRenter').controller('navbarController', navbarController);
-  navbarController.$inject = ['$log', 'constants', 'generalService', '$localForage'];
+  navbarController.$inject = ['$log', 'constants', 'generalService',
+  '$localForage', 'database'];
 
-  function navbarController($log, constants, generalService, $localForage) {
+  function navbarController($log, constants, generalService, $localForage, database) {
     var vm = this;
     //vm.name = "navbarController name"; //wtf does this do?
     //vm.swapMessage = "..."; //wtf does THIS do?
@@ -14,6 +15,7 @@
     vm.refresh = refresh;
     vm.go = go;
     vm.back = back;
+    vm.whosClockedIn = whosClockedIn;
     vm.quickPrint = quickPrint;
     //vm.swapRooms;
 
@@ -46,6 +48,15 @@
                });
             });
           }
+      });
+    }
+    function whosClockedIn() {
+      $log.log("Getting whos logged in");
+      database.getUsersLoggedIn(function(Users) {
+        $log.log("users: " + JSON.stringify(Users));
+        Users.forEach(function(User){
+          $log.log("  user: " + JSON.stringify(User));          
+        });
       });
     }
     /*theres a build rooms function, I don't need it I guess*/
